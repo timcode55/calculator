9c1033212132e98928901e7f92486e47cee0bbb3
Tim Petrvalsky <tpet55@yahoo.com>
2020-03-26 20:13:42 -0700

    Basics are working

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+let entryArea = document.getElementById('top-data');
+
+function clickBtn(obj) {
+	// console.log(this);
+	var press = obj.innerHTML;
+
+	if (press == '=') {
+		// Calculate
+		entryArea.innerHTML = eval(entryArea.innerHTML);
+	} else if (press == 'AC') {
+		// All Clear
+		entryArea.innerHTML = '0';
+	} else {
+		if (entryArea.innerHTML == '0') {
+			entryArea.innerHTML = press;
+		} else {
+			entryArea.innerHTML += press;
+		}
+	}
+}
